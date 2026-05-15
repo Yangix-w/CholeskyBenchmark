c0871a6161914c19edfd5eb9702df2b734bf9540
Yangix <l.shi1@campus.unimib.it>
2026-05-15 15:57:09 +0200

    Modified sleep every 5ms

--- matlab/monitor_memory.ps1
+++ matlab/monitor_memory.ps1
@@ -16,5 +16,5 @@ while (-not (Test-Path -Path $FlagFile)) {
         $memKB = [math]::Round($process.WorkingSet64 / 1024)
         $memKB | Out-File -FilePath $LogFile -Append
     }
-    Start-Sleep -Milliseconds 10 # Campiona ogni 10ms
+    Start-Sleep -Milliseconds 5 # Campiona ogni 5ms
 }
--- matlab/monitor_memory.sh
+++ matlab/monitor_memory.sh
@@ -11,7 +11,7 @@ FLAGFILE=$3
 while [ ! -f "$FLAGFILE" ]; do
     # Estrae la memoria RSS in KB per il PID e la salva nel log
     ps -p $PID -o rss= >> "$LOGFILE"
-    sleep 0.01 # Campiona ogni 10ms
+    sleep 0.005 # Campiona ogni 5ms
 done
 
 # Lancia chmod +x monitor_memory.sh da terminale prima di avviare MATLAB
